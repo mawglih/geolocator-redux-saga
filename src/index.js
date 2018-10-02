a6039e5fb3455926249bc44ea78458f20a821643
Oleg Markoff <mawglih@gmail.com>
2018-10-01 20:32:19 -0400

    initial

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import {
+  applyMiddleware,
+  compose,
+  createStore,
+} from 'redux';
+import reducer from 'reducers';
+import createSagaMiddleware from 'redux-saga';
+import Geo from 'containers/Geo';
+import {
+  watchFetch,
+} from 'sagas'; //eslint-disable-next-line
+import styles from './index.css';
+
+import registerServiceWorker from './registerServiceWorker';
+
+const logger = store => {
+  return next => {
+    return action => {
+      console.log('[Midlleware] dispatching', action);
+      next(action);
+    }
+  }
+}
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducer,
+  composeEnhancer(
+    applyMiddleware(
+      sagaMiddleware,
+      logger,
+    )
+  )
+);
+sagaMiddleware.run(watchFetch);
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Geo />
+  </Provider>
+  , document.getElementById('root'));
+registerServiceWorker();
